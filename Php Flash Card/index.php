badb25b8adadc3c01f23e792455d1ec70b53bc38
Utsav Khanal <100432431+Utsav-Khanal@users.noreply.github.com>
2023-08-04 20:46:23 +0545

    Add files via upload

---
+++ Php Flash Card/index.php
@@ -0,0 +1,34 @@
+<?php
+
+        $number = filter_input(INPUT_GET, 'num', FILTER_SANITIZE_NUMBER_INT);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multilication Flash Cards</title>
+    <link rel="stylesheet" href="css/main.min.css">
+</head>
+<body>
+
+
+            <?php include("./view/header.php"); ?>
+            <?php
+            if ($number)
+            {
+                include("view/results.php");
+            }
+            else{
+                include("view/forms.php");
+            }
+
+            ?>
+            <?php include("./view/footer.php"); ?>
+
+
+</body>
+</html>
